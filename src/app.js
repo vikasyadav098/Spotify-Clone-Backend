480f28878df2991cd1ba6281208ee3542c63a8ac
vikasyadav098 <vikyadv098@gmail.com>
2026-04-29 14:10:16 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors =require("cors")
 
 const app= express()
 app.use(cors({
-   origin: "http://localhost:5173",
+   origin: "https://spotify-mern-stack-project-iypejtg0i-vikasyadav098s-projects.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
